9e84d434059e5d4e872392ef005f37a3ca99166c
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-10-22 17:29:31 -0500

    Clase Figura

---
+++ src/actividadunidad_3/Figura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Figura {
+    public void CalcularArea(){
+        System.out.println("Calculando area....");
+    }
+    
+}
