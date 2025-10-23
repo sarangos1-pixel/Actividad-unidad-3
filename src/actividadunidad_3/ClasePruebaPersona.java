bff1e26c24e9a136a8474f142b6d386d96c38c3e
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-10-23 17:06:20 -0500

    Clase main Persona ejercicio 2

---
+++ src/actividadunidad_3/ClasePruebaPersona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ClasePruebaPersona {
+
+    
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        Persona estudiante1 = new Estudiante();
+        Persona profesor1 = new Profesor ();
+        
+        persona1.Presentarse();
+        estudiante1.Presentarse();
+        profesor1.Presentarse();
+        
+        
+        
+    }
+    
+}
