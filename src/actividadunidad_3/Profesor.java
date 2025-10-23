6471144ffb168ad9bbe786112a500d7eed718edb
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-10-23 17:05:37 -0500

    Clase derivada Profesor ejercicio 2

---
+++ src/actividadunidad_3/Profesor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+
+public class Profesor extends Persona {
+    @Override
+    public void Presentarse(){
+        System.out.println("Me presento, soy un profesor");
+    }
+    
+}
