1c958be759064f8f432f2cf2e454687df95b76d0
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-10-23 17:04:11 -0500

    Clase Persona ejercicio 2

---
+++ src/actividadunidad_3/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+
+public class Persona {
+    public void Presentarse(){
+        System.out.println("Hola me presento");
+    }
+    
+}
