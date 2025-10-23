e18efd0bf7b6470b742b0689e614390625fb5605
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-10-23 17:05:10 -0500

    Clase derivada Estudiante ejercicio 2

---
+++ src/actividadunidad_3/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+
+public class Estudiante extends Persona {
+    @Override
+    public void Presentarse(){
+        System.out.println("Me presento, soy un estudiante");
+    }
+    
+}
