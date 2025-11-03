953ea2f5dbc748ac2f4194a37e5b8292bf6fc9fc
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:20:19 -0500

    Clase Bicicleta ejercicio 4

---
+++ src/actividadunidad_3/Bicicleta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Bicicleta extends Vehiculo {
+    @Override
+    public void Mover(){
+        System.out.println("la bicicleta se esta moviendo");
+    }
+    
+}
