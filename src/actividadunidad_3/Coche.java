d270a4971135540853a7ae1abd18c076f35cdef7
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:18:35 -0500

    Clase coche ejercicio 4

---
+++ src/actividadunidad_3/Coche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Coche extends Vehiculo {
+    @Override
+    public void Mover(){
+        System.out.println("el coche se esta moviendo");
+    }
+    
+}
