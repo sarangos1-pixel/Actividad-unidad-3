120564e98656417133b1c10972126ec505cfd1b1
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:23:44 -0500

    Ejemplo ejercicio 3

---
+++ src/actividadunidad_3/Instrumento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Instrumento {
+    public void HacerSonido(){
+        System.out.println("Toca notas musicales ");
+    }
+}
