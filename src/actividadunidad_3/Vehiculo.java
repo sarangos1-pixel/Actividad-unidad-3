1b2bde24acbd91c1448d7d3429c301cff865fd53
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:14:57 -0500

    Clase vehiiculo ejercicio 4

---
+++ src/actividadunidad_3/Vehiculo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Vehiculo {
+    public void Mover(){
+        System.out.println("el vehiculo se esta movimiendo");
+    }
+    
+}
