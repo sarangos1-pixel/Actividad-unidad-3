ff99eede229e5e464d471ebb105e7f53f9e67cb0
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:20:54 -0500

    Clase man Vehiculo ejercicio 4

---
+++ src/actividadunidad_3/ClasePruebaVehiculo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ClasePruebaVehiculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vehiculo coche1 = new Coche();
+        Vehiculo Bicicleta1 = new Bicicleta();
+        
+        coche1.Mover();
+        Bicicleta1.Mover();
+        
+        
+    }
+    
+}
