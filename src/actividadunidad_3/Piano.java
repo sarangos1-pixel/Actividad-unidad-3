4b1c43aa54bc0aad99cb5a62028fd62f05146b21
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-03 15:26:35 -0500

    Ejemplo ejercicio 3

---
+++ src/actividadunidad_3/Piano.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadunidad_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Piano extends Instrumento {
+    
+   @Override
+    public void Sonar(){
+        System.out.println("*** tocar piano ***");  
+    }
+    
+}
